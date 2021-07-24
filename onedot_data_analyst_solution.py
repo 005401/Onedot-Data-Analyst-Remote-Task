2be5538a35a18a9ba0f02d28651b1ad05b8fe7f8
Karol Nass <karol.nass@gmail.com>
2021-07-24 11:30:47 +0200

    clean 2

--- onedot_data_analyst_solution.py
+++ onedot_data_analyst_solution.py
@@ -2,60 +2,50 @@
 # coding: utf-8
 
 import pandas as pd
-import numpy as np
 import json
-from collections import Counter
 
-
-# ## Take a look at the input data
 input_data = 'supplier_car.json'
 
 #read input json file, take care of the encoding!
-with open(input_data, encoding='utf-8') as f:
-    df = pd.DataFrame([json.loads(line) for line in f.readlines()])
+d = [json.loads(line) for line in open(input_data, 'r', encoding='utf-8')]
+df = pd.DataFrame(data=d)
 
 #convert ID column to numeric type and sort it
-df.ID = pd.to_numeric(df.ID)
+df['ID'] = df['ID'].astype('float')
 df = df.sort_values("ID")
 
-# ## Pre-processing
+# Pre-processing
 # I need to transform the supplier data to achieve the same granularity as the target data
 
 # Some rows in the input data have repeting ID and column values, while only the Attribute Names and Attribute Values change.
 # We can group them and then make new columns with all Attribute Names and Attribute Values in single row for each ID
-
-CONSTANT_COLUMNS = ["ID", "MakeText", "TypeName", "TypeNameFull", "ModelText", "ModelTypeText"]
+df_grp = df.groupby(["ID", "MakeText", "TypeName", "TypeNameFull", "ModelText", "ModelTypeText"])
+df_grp_agg = df_grp.agg({"Attribute Names": list, "Attribute Values": list}).reset_index()
 
 # define function to aggragate attributes
-def aggregate_attributes(row):
+def aggr_attr(row):
     attr = {key: value for key, value in zip(row["Attribute Names"], row["Attribute Values"])}
-    attr.update({col_name: row[col_name] for col_name in CONSTANT_COLUMNS})
+    attr.update({col_name: row[col_name] for col_name in ["ID", "MakeText", "TypeName", "TypeNameFull", "ModelText", "ModelTypeText"]})
     return pd.Series(attr)
 
-# aggregare input data to create one row per product
-agg_data = df.groupby(CONSTANT_COLUMNS).agg(
-    {"Attribute Names": list, "Attribute Values": list}
-).reset_index().apply(aggregate_attributes, axis=1)
+# Perform aggragate
+df_grp_agg_attr = df_grp_agg.apply(aggr_attr, axis=1)
 
 
-
-# ## Normalization
+# Normalization
 # Normalisation is required in case an attribute value is different but actually is the same (different
 # spelling, language, different unit used etc.).
 # 
 # E.g. the first column in the target data "carType" defines cat Types (	Coupé	Convertible / Roadster	Other etc.)
 # The column BodyTypeText in the input data can be used as carTypes for the target data 
-# but it uses slightly different carType names (some in German) and some are missgng (e.g. Single seater)
-# One could use the number of seats to find the "Single seater" cars for the target data
+# but it uses slightly different names (some are in German) and some are missgng (e.g. Single seater)
+# We could use the number of seats to find the "Single seater" cars for the target data
 
-
-# ### Normalize column "carType"
-
+# Normalize column "carType"
 # it is neccessary to change the names of the car types found in BodyTypeText column of the input data to match
 # the names used in Target Data carType column and use the number of seats to find the "Single seater". 
 # If the BodyType in input data can't be assigned to one of the types in target data, put it to "Other"
-
-def normalize_cartype(row):
+def norm_cartype(row):
     if row["Seats"] == 1:
         return "Single seater"
     try:
@@ -69,24 +59,21 @@ def normalize_cartype(row):
     except KeyError:
         return "Other"
 
-
-# ### Normalize column "color"
-# the same as with carType. Some color are in German and some are slightly different. Assign them to standard values from target data
-
-def normalize_color(row):
-    for english, germans in [
-            ("Black", ["schwarz"]), ("Silver", ["silber"]), ("Blue", ["blau"]), ("Gray", ["grau", "anthrazit"]), ("White", ["weiss"]),
-            ("Red", ["red", "bordeaux"]), ("Green", ["grün"]), ("Yellow", ["gelb"]), ("Purple", ["violett"]),
-            ("Gold", ["gold"]), ("Brown", ["braun"]), ("Orange", ["orange"]), ("Beige", ["beige"])]:
-        for german in germans:
-            if german in row["BodyColorText"]:
-                return english
+# Normalize column "color"
+# the same as with carType. Some colors are in German and some are slightly different. 
+# Assign them to "standard" values from target data
+def norm_color(row):
+    for color_en, color_de in [
+            ("Black", ["schwarz"]), ("Silver", ["silber"]), ("Blue", ["blau"]), ("Gray", ["grau", "anthrazit"]), 
+            ("White", ["weiss"]), ("Red", ["red", "bordeaux"]), ("Green", ["grün"]), ("Yellow", ["gelb"]), 
+            ("Purple", ["violett"]), ("Gold", ["gold"]), ("Brown", ["braun"]), ("Orange", ["orange"]), ("Beige", ["beige"])]:
+        for item in color_de:
+            if item in row["BodyColorText"]:
+                return color_en
     return "Other"
 
-
-# ### Normalize column ConditionTypeText
-
-def normalize_condition(row):
+# Normalize column ConditionTypeText
+def norm_condition(row):
     try:
         return {
             "Occasion": "Used",
@@ -96,25 +83,20 @@ def normalize_condition(row):
         }[row["ConditionTypeText"]]
     except KeyError:
         return "Other"
-
-
-# ### Normalize columns model and model_variant
-# In order to extract  variant  without  model, we can remove  model  from the column ModelTypeText. 
-# If this doesn't work, we use TypeName as a fallback. Note: When there is no variant, 
-# this will result in an empty string.
-
-def normalize_variant(row):
+    
+# Normalize columns model and model_variant
+# In order to extract  variant we can remove  model  from the column ModelTypeText. 
+# If this doesn't work, we use TypeName.
+def norm_variant(row):
     model = row["ModelText"].strip()
     model_variant = row["ModelTypeText"].strip()
     if model_variant[:len(model)].lower() == model.lower():
         return model_variant[len(model):].strip()
     return row["TypeName"]
 
-
-# ### Normalize column Zip
-# Can be done automatically with e.g. pyzipcode.
-
-def normalize_zip(row):
+# Normalize column Zip
+# Can be done automatically with e.g. pgeocode
+def norm_zip(row):
     try:
         return {
             "Zuzwil": "9524",
@@ -128,36 +110,30 @@ def normalize_zip(row):
         return "Other"
 
 
-# ## Perform normalization of all columns posible to normalize
-
-
-NORM_FUNCTIONS = {
-    "BodyTypeText": normalize_cartype,
-    "BodyColorText": normalize_color,
-    "Condition": normalize_condition,
-    "Variant": normalize_variant,
-    "Zip": normalize_zip,
-
+# Perform normalization of all columns posible to normalize
+NORM_FUNCT = {
+    "BodyTypeText": norm_cartype,
+    "BodyColorText": norm_color,
+    "Condition": norm_condition,
+    "Variant": norm_variant,
+    "Zip": norm_zip,
 }
 
 def normalize(row):
-    for column, funct in NORM_FUNCTIONS.items():
+    for column, funct in NORM_FUNCT.items():
         row[column] = funct(row)
     return row
     
+# perform normalization    
+normalized_df = df_grp_agg_attr.apply(normalize, axis=1)
 
-normalized_data = agg_data.apply(normalize, axis=1)
-normalized_data.head()
-
-
-# ## Perform integration
-
+# Define finction to integrate normalized data into the target data format
 def integrate(row):
     return pd.Series({
         "carType": row["BodyTypeText"],
         "color": row["BodyColorText"],
         "condition": row["ConditionTypeText"],
-        "currency": "CHF", # assume all cars are from/to be sold CH
+        "currency": "CHF", # assume that all cars are from/to be sold CH
         "drive": "LHD", # all cars in the input data are from CH, hence LHD, could not find column to normalize
         "city": row["City"], # all cities are from CH
         "country": "CH", # all cars in the input data are from CH but can be deduced from the city name
@@ -174,13 +150,13 @@ def integrate(row):
         "fuel_consumption_unit": "l_km_consumption" if row["ConsumptionTotalText"] and row["ConsumptionTotalText"] != 'null' else None,
     })
 
-formated_data = normalized_data.apply(integrate, axis=1)
-formated_data.head(100)
+# perform data integration
+integrated_df = normalized_df.apply(integrate, axis=1)
+
+# Write Excel file with three tabls containing results of each step above
+with pd.ExcelWriter("onedot_data_analyst_solution.xlsx") as writer:
+    df_grp_agg_attr.to_excel(writer, sheet_name="Pre-processed Data", index=False, na_rep="null")
+    normalized_df.to_excel(writer, sheet_name="Normalized Data", index=False, na_rep="null")
+    integrated_df.to_excel(writer, sheet_name="Integrated Data", index=False, na_rep="null")
 
 
-# ## Export Excel files
-
-with pd.ExcelWriter("solution.xlsx") as writer:
-    agg_data.to_excel(writer, sheet_name="Pre-processed Data", index=False, na_rep="null")
-    normalized_data.to_excel(writer, sheet_name="Normalized Data", index=False, na_rep="null")
-    formated_data.to_excel(writer, sheet_name="Integrated Data", index=False, na_rep="null")
